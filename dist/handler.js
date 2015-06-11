80e892d9c32e527ad10c4642b07409a386f431dd
baio <max.putilov@gmail.com>
2015-06-11 17:53:59 +0300

    fix

--- dist/handler.js
+++ dist/handler.js
@@ -3,9 +3,25 @@ var request = require("request");
 function handle(log, pubHub) {
     log.write({ oper: "handle", status: "start" });
     console.log(">>>handler.ts:6", process.env.IMPORTIO_URL);
-    request(process.env.IMPORTIO_URL, function (err, resp, body) {
+    request.get({ url: process.env.IMPORTIO_URL, json: true }, function (err, resp, body) {
         if (!err) {
-            console.log(">>>handler.ts:8", body);
+            log.write({ oper: "handle", status: "received" });
+            console.log(">>>handler.ts:10", body);
+            var data = body.results.map(function (m) {
+                return {
+                    "id": m.value_1 + "_" + m.value_2 + "_" + m.value_3,
+                    "source": "silver-surfer",
+                    data: {
+                        "time": m.value_1,
+                        "ticket": m.value_2,
+                        "oper": m.value_3 == "Продавать" ? "sell" : "buy",
+                        "stop": m.number ? m.number : null,
+                        "force": m.value_4 && m.value_4 != "Штиль" ? parseInt(m.value_4) : null
+                    }
+                };
+            });
+            log.write({ oper: "handle", status: "mapped", data: data });
+            data.forEach(function (m) { return pubHub.pub(m); });
         }
     });
 }
--- src/handler.ts
+++ src/handler.ts
@@ -4,11 +4,29 @@ var request = require("request");
 export function handle (log: logger.ILogger, pubHub: pubSub.IPubSub) {
   log.write({oper: "handle", status: "start"});
   console.log(">>>handler.ts:6", process.env.IMPORTIO_URL);
-  request(process.env.IMPORTIO_URL, (err, resp, body) => {
+  request.get({url: process.env.IMPORTIO_URL, json: true}, (err, resp, body) => {
     if (!err) {
-      console.log(">>>handler.ts:8", body);
+      log.write({oper: "handle", status: "received"});
+      console.log(">>>handler.ts:10", body);
+      var data = body.results.map((m: any) => {
+        return {
+          "id" : m.value_1 + "_" + m.value_2 + "_" + m.value_3,
+          "source" : "silver-surfer",
+          data : {
+            "time": m.value_1,
+            "ticket": m.value_2,
+            "oper": m.value_3 == "Продавать" ? "sell" : "buy",
+            "stop": m.number ? m.number : null,
+            "force": m.value_4 && m.value_4 != "Штиль" ? parseInt(m.value_4) : null
+          }
+        }
+      });
+
+      log.write({oper: "handle", status: "mapped", data : data});
+      data.forEach((m) => pubHub.pub(m));
+
     }
   });
-  //pubHub.pub();
+
 }
 
